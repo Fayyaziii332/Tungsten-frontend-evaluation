c120d26131aec28e3c91791691339914e876aa4b
Fayyaziii332 <54409264+Fayyaziii332@users.noreply.github.com>
2022-02-15 14:25:56 +0500

    laid the base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css';
+
+const App = () => {
+  return (
+    <div>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FormModal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FormModal = () => {
+  return (
+    <></>
+  );
+};
+
+export default FormModal;
